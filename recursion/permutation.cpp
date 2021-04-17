edcc77ef1ebfaa0cd0f25b4a7fc213f875f67baa
aj <ankurjain41282@gmail.com>
2021-04-17 07:21:53 -0700

    made changes to permutation code to work

--- recursion/permutation.cpp
+++ recursion/permutation.cpp
@@ -1,31 +1,39 @@
 using namespace std;
 #include<iostream>
-void generateperm(int*array,int size,int counter)
+void generateperm(int*array,int sizeofarray,int counter)
 {
-  int countery=0;
-  if(counter<=size)
+  int countery=0,x;
+  if(counter==sizeofarray)
     {
-      return;
-    }
-  while(countery<=counter)
+      for(countery=0;countery<sizeofarray;countery++)
+	{
+	  std::cout<<array[countery];
+	}
+      std::cout<<endl;
+     }
+  //  generateperm(array+1,sizeofarray,counter+1);
+  /* while(countery<=counter)
     {
       std::cout<<array[countery];
+      countery++;
     }
-  generateperm(array+1,size,counter+1);
+  std::cout<<endl;
+  generateperm(array+1,sizeofarray,counter+1);*/
+   for(x=counter;x<sizeofarray;x++)
+    {
+        swap(array[0],array[x]);
+	generateperm(array,sizeofarray,counter+1);
+	swap(array[x],array[0]);
+    }
+
 }
 int main()
 {
   int array[]={1,2,3};
-  int sizeofarray,x,counter=0;
+  int sizeofarray=0,counter=0;
   sizeofarray=sizeof(array)/sizeof(int);
-  //generateperm(array,sizeofarray);
-  for(x=0;x<sizeofarray;x++)
-    {
-        swap(array[0],array[x]);
-	generateperm(array,sizeofarray,counter);
-	swap(array[x],array[0]);
-    }
-
+  generateperm(array,sizeofarray,counter);
+ 
 
 
 }
