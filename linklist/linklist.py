7a0bc87fe62944055f80b8e682f1b025ca46f157
aj <ankurjain41282@gmail.com>
2021-11-20 10:48:23 -0800

    added code of linlist.py to linklist.py

---
+++ linklist/linklist.py
@@ -0,0 +1,7 @@
+#/usr/bin/python3
+#code for implementing linklist in python
+def insertl(element):
+    print("inserting element into linklist")
+
+if(__name__="__main__"):
+    print("linklist work")
--- linklist/linlist.py
+++
@@ -1,7 +0,0 @@
-#/usr/bin/python3
-#code for implementing linklist in python
-def insertl(element):
-    print("inserting element into linklist")
-
-if(__name__="__main__"):
-    print("linklist work")
