0a54eb7347d1086952b9d44efba6e56e2d979c47
aj <ankurjain41282@gmail.com>
2021-11-22 04:33:54 -0800

    circular linklist

--- linklist/clinklist.c
+++ linklist/clinklist.c
@@ -6,50 +6,67 @@ struct node{
   struct node* next;
 };
 
-int cinsert(struct node**,struct node*,int value);
+int cinsert(struct node**,struct node**,int value);
 int cdelete(struct node*,int position);
-int cview(struct node*);
-
-
+int cview(struct node*,struct node*);
+// struct node*rear;
+//rear=NULL;
 int main()
 {
   struct node*front;
-  struct node*rear;
+
   int element[]={1,2,3,4,5};
   int counter=0;
+  struct node *rear;
   int sizeofelement=sizeof(element)/sizeof(int);
+  printf("%d",sizeofelement);
   //printf("enter the element to be inserted");
   //scanf("%d",&element);
   front=(struct node*)malloc(sizeof(struct node*));
-  front->next=NULL;
+  front=NULL;
+  rear=(struct node*)malloc (sizeof(struct node*));
   rear=front;
   
   for(counter=0;counter<sizeofelement;counter++)
     {
-      cinsert(&front,rear,element[counter]);
+      cinsert(&front,&rear,element[counter]);
     }
-
+  cview(front,rear);
 }
 
 
- int cinsert(struct node** front,struct node* rear,int value)
+int cinsert(struct node** front,struct node**rear,int value)
  {
    struct node*new1;
    new1=(struct node*)malloc(sizeof(struct node*));
    new1->element=value;
-   if((*front)->next==NULL)
+   if(*rear==NULL)
      {
-     
-       (*front)->element=value;
-       (*front)->next=new1;
+       (*front)=(*rear)=new1;
+        // (*front)->element=value;
+       //(*front)->next=new1;
+       
+       // new1->next=*front;
      }
    else
      {
-        new1->element=value;
-       rear->next=new1;
+       
+
+       // new1->element=value;
+       (*rear)->next=new1;
        
      }
-       rear=new1;
-       new1->next=*front;
+   *(rear)=new1;
+   (*rear)->next=*front;
        return 0;
  }
+int cview(struct node*front,struct node*rear)
+{
+  struct node*temp;
+  temp=front;
+  while(temp->next!=front)
+    {
+      printf("%d\n",temp->element);
+	temp=temp->next;
+    }
+}
